f637c1215d7158d55c4447a4e2e7d0e1190184e4
Ittipat Pattum <nampu@finema.co>
2024-03-02 17:34:51 +0700

    save

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,41 @@
-use axum::{routing::get, Router};
+use app_789plates_server::authentication::Email;
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use email_address::EmailAddress;
 use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() {
-    let pool = PgPool::connect("postgres://postgres:mysecretpassword@localhost:5423/app789plates")
+    let pool = PgPool::connect("postgres://postgres:mysecretpassword@localhost:5432/app789plates")
         .await
         .unwrap();
-    let app = Router::new().route("/", get(|| async {})).with_state(pool);
+    let app = Router::new()
+        .route("/", get(|| async {}))
+        .route("/verifyemail", post(verify_email))
+        .with_state(pool);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8700").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn verify_email(
+    State(state): State<PgPool>,
+    Json(payload): Json<Email>,
+) -> (StatusCode, Json<Option<Email>>) {
+    let valid = EmailAddress::is_valid(&payload.email.trim().to_lowercase());
+    if valid {
+        // check exist in db
+        (
+            StatusCode::OK,
+            (Json(Some(Email {
+                email: payload.email.trim().to_lowercase(),
+            }))),
+        )
+    } else {
+        (StatusCode::FORBIDDEN, (Json(None)))
+    }
 }
