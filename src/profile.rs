54f6a5f042a6bf94794f582dccc7fea5e18f3ac9
Ittipat Pattum <nampu@finema.co>
2024-03-10 21:03:25 +0700

    save

--- src/profile.rs
+++ src/profile.rs
@@ -1,5 +1,32 @@
-use axum::response::IntoResponse;
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use jsonwebtoken::{decode, DecodingKey, TokenData, Validation};
+use serde::Deserialize;
+use sqlx::PgPool;
 
-pub async fn edit_name() -> impl IntoResponse {}
+use crate::jwt::{Claims, REFRESH_TOKEN_KEY};
+
+#[derive(Deserialize)]
+pub struct EditName {
+    pub refresh_token: String,
+    pub name: String,
+}
+
+pub async fn edit_name(
+    State(pool): State<PgPool>,
+    Json(payload): Json<EditName>,
+) -> impl IntoResponse {
+    let token = decode::<Claims>(
+        &payload.refresh_token,
+        &DecodingKey::from_secret(REFRESH_TOKEN_KEY.as_ref()),
+        &Validation::default(),
+    );
+    if let Ok(TokenData { header: _, claims }) = token {
+        let users_id: i32 = claims.sub.parse().unwrap();
+        // do things
+        StatusCode::OK
+    } else {
+        StatusCode::UNAUTHORIZED
+    }
+}
 pub async fn edit_profile_picture() -> impl IntoResponse {}
 pub async fn edit_information() -> impl IntoResponse {}
