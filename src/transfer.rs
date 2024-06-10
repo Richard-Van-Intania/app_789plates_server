0715d631cf337c34ea1ccc0bb601de5756fb94b4
Ittipat Pattum <nampu@finema.co>
2024-06-10 22:48:39 +0700

    save

--- src/transfer.rs
+++ src/transfer.rs
@@ -1,5 +1,6 @@
 use crate::app_state::AppState;
 use axum::{extract::State, Json};
+use chrono::Utc;
 use hyper::StatusCode;
 use serde::{Deserialize, Serialize};
 
@@ -18,6 +19,28 @@ pub async fn transfer_plates(
     State(AppState { pool, client: _ }): State<AppState>,
     Json(payload): Json<Transfer>,
 ) -> StatusCode {
-    //
-    StatusCode::OK
+    let add_date = Utc::now();
+    let insert = sqlx::query("INSERT INTO public.transfer_plates(plates_id, users_id, store_id, add_date) VALUES ($1, $2, $3, $4)")
+        .bind(payload.plates_id)
+        .bind(payload.users_id)
+        .bind(payload.store_id)
+        .bind(add_date)
+        .fetch_optional(&pool)
+        .await;
+    match insert {
+        Ok(ok) => match ok {
+            Some(_) => StatusCode::OK,
+            None => StatusCode::BAD_REQUEST,
+        },
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+}
+
+pub async fn accept_plates(
+    State(AppState { pool, client: _ }): State<AppState>,
+    Json(payload): Json<Transfer>,
+) -> StatusCode {
+    // change user
+    // update transfer
+    StatusCode::INTERNAL_SERVER_ERROR
 }
