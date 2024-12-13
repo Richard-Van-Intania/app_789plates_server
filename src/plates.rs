cafd62aa046fc22aea005181acd430deb1580a11
Ittipat Pattum <nampu@finema.co>
2024-12-13 20:05:23 +0700

    add all like save

--- src/plates.rs
+++ src/plates.rs
@@ -1,4 +1,8 @@
-use crate::{app_state::AppState, pattern::analyze_pattern, query::PlatesFilter};
+use crate::{
+    app_state::AppState,
+    pattern::analyze_pattern,
+    query::{PlatesFilter, UsersFilter},
+};
 use axum::{extract::State, Json};
 use chrono::Utc;
 use hyper::StatusCode;
@@ -361,6 +365,76 @@ pub async fn remove_saved_plates(
         sqlx::query("DELETE FROM public.saved_plates WHERE (users_id = $1 AND plates_id = $2)")
             .bind(payload.users_id)
             .bind(payload.plates_id)
+            .execute(&pool)
+            .await;
+    match delete {
+        Ok(_) => StatusCode::OK,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+}
+
+pub async fn add_liked_store(
+    State(AppState { pool, client: _ }): State<AppState>,
+    Json(payload): Json<UsersFilter>,
+) -> StatusCode {
+    let add_date = Utc::now();
+    let insert = sqlx::query(
+        "INSERT INTO public.liked_store(users_id, store_id, add_date) VALUES ($1, $2, $3)",
+    )
+    .bind(payload.users_id)
+    .bind(payload.store_id)
+    .bind(add_date)
+    .execute(&pool)
+    .await;
+    match insert {
+        Ok(_) => StatusCode::OK,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+}
+
+pub async fn remove_liked_store(
+    State(AppState { pool, client: _ }): State<AppState>,
+    Json(payload): Json<UsersFilter>,
+) -> StatusCode {
+    let delete =
+        sqlx::query("DELETE FROM public.liked_store WHERE (users_id = $1 AND store_id = $2)")
+            .bind(payload.users_id)
+            .bind(payload.store_id)
+            .execute(&pool)
+            .await;
+    match delete {
+        Ok(_) => StatusCode::OK,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+}
+
+pub async fn add_saved_store(
+    State(AppState { pool, client: _ }): State<AppState>,
+    Json(payload): Json<UsersFilter>,
+) -> StatusCode {
+    let add_date = Utc::now();
+    let insert = sqlx::query(
+        "INSERT INTO public.saved_store(users_id, store_id, add_date) VALUES ($1, $2, $3)",
+    )
+    .bind(payload.users_id)
+    .bind(payload.store_id)
+    .bind(add_date)
+    .execute(&pool)
+    .await;
+    match insert {
+        Ok(_) => StatusCode::OK,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+}
+
+pub async fn remove_saved_store(
+    State(AppState { pool, client: _ }): State<AppState>,
+    Json(payload): Json<UsersFilter>,
+) -> StatusCode {
+    let delete =
+        sqlx::query("DELETE FROM public.saved_store WHERE (users_id = $1 AND store_id = $2)")
+            .bind(payload.users_id)
+            .bind(payload.store_id)
             .execute(&pool)
             .await;
     match delete {
